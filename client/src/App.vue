51945aa0a4c06a23878ecdd14a85f1f03ed8c0db
sstehniy <sstehniy@gmail.com>
2020-06-05 13:01:54 +0300

    basic folder structure, linter/formatter settings, hello-world boilerplate

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-size: 16px;
+  font-family: sans-serif;
+}
+
+#app {
+  background-color: lightcoral;
+  min-height: 100vh;
+  min-width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 id="title">Hello Kot</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+};
+</script>
+
+<style scoped>
+#title {
+  padding: 50px 80px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.351), 0 0 12px rgba(0, 0, 0, 0.235),
+    0 0 13px rgba(0, 0, 0, 0.276), 0 0 20px rgba(0, 0, 0, 0.187);
+  background-color: antiquewhite;
+  color: lightseagreen;
+  letter-spacing: 5px;
+  text-transform: uppercase;
+  font-size: 2.5rem;
+  text-shadow: 5px 0 chocolate;
+}
+</style>
